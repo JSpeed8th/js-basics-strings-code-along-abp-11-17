f695ecf5110bfc3cd703142e32f36548ee3db00a
JSpeed8th <jspeed8th@gmail.com>
2017-11-18 19:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
+<<<<<<< HEAD
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+=======
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+>>>>>>> afc434dc5430aed56355e2b229816e9c001188f6
